968eb60e788d5d02c884cbb819fc36eb782e0d24
NotFounds <m134_warrock_rpg7@yahoo.co.jp>
2017-11-16 14:17:33 +0900

    Add a classification model and Use the weight trained by the model

--- model.py
+++ model.py
@@ -4,10 +4,9 @@ import chainer.links as L
 import numpy as np
 
 # Network definition
-class Colorization(chainer.Chain):
-
+class Classification(chainer.Chain):
     def __init__(self, map_n, out_n):
-        super(Colorization, self).__init__(
+        super(Classification, self).__init__(
             conv1=L.Convolution2D(None, 1*map_n, 4, 2, 1),
             conv2=L.Convolution2D(None, 2*map_n, 4, 2, 1),
             conv3=L.Convolution2D(None, 4*map_n, 4, 2, 1),
@@ -16,6 +15,38 @@ class Colorization(chainer.Chain):
             conv6=L.Convolution2D(None, 8*map_n, 4, 2, 1),
             conv7=L.Convolution2D(None, 16*map_n, 4, 2, 1),
             conv8=L.Convolution2D(None, 16*map_n, 4, 2, 1),
+
+            l1 = L.Linear(None, 64),
+            l2 = L.Linear(None, 32),
+            l3 = L.Linear(None, out_n),
+        )
+        self.map_n = map_n
+        self.out_n = out_n
+
+    def __call__(self, x):
+        y = self.conv1(x)
+        y = self.conv2(F.relu(y))
+        y = self.conv3(F.relu(y))
+        y = self.conv4(F.relu(y))
+        y = F.dropout(self.conv5(F.relu(y)))
+        y = F.dropout(self.conv6(F.relu(y)))
+        y = F.dropout(self.conv7(F.relu(y)))
+        y = F.dropout(self.conv8(F.relu(y)))
+        y = self.l1(F.relu(y))
+        y = self.l2(F.relu(y))
+        return self.l3(F.relu(y))
+
+class Colorization(chainer.Chain):
+    def __init__(self, map_n, out_n, net=None):
+        super(Colorization, self).__init__(
+            conv1=L.Convolution2D(None, 1*map_n, 4, 2, 1, initialW=net.conv1.W.data if net else None, initial_bias=net.conv1.b.data if net else None),
+            conv2=L.Convolution2D(None, 2*map_n, 4, 2, 1, initialW=net.conv2.W.data if net else None, initial_bias=net.conv2.b.data if net else None),
+            conv3=L.Convolution2D(None, 4*map_n, 4, 2, 1, initialW=net.conv3.W.data if net else None, initial_bias=net.conv3.b.data if net else None),
+            conv4=L.Convolution2D(None, 8*map_n, 4, 2, 1, initialW=net.conv4.W.data if net else None, initial_bias=net.conv4.b.data if net else None),
+            conv5=L.Convolution2D(None, 8*map_n, 4, 2, 1, initialW=net.conv5.W.data if net else None, initial_bias=net.conv5.b.data if net else None),
+            conv6=L.Convolution2D(None, 8*map_n, 4, 2, 1, initialW=net.conv6.W.data if net else None, initial_bias=net.conv6.b.data if net else None),
+            conv7=L.Convolution2D(None, 16*map_n, 4, 2, 1, initialW=net.conv7.W.data if net else None, initial_bias=net.conv7.b.data if net else None),
+            conv8=L.Convolution2D(None, 16*map_n, 4, 2, 1, initialW=net.conv8.W.data if net else None, initial_bias=net.conv8.b.data if net else None),
 
             dconv1=L.Deconvolution2D(None, 16*map_n, 4, 2, 1),
             dconv2=L.Deconvolution2D(None, 16*map_n, 4, 2, 1),
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import sys
 import copy
 import time
+import random
 import argparse
 import datetime
 import numpy as np
@@ -11,7 +12,13 @@ try:
 except ImportError:
     pass
 
+try:
+    from chainer import cuda
+except ImportError:
+    pass
+
 import chainer
+import chainer.links as L
 import chainer.functions as F
 from chainer import datasets
 from chainer import training
@@ -22,107 +29,163 @@ import matplotlib.pyplot as plt
 import model as M
 import util
 
+class DelGrad(object):
+    name = 'DelGrad'
+    def __init__(self, target):
+        self.target = target
+
+    def __call__(self, optimizer):
+        for name, param in optimizer.target.namedparams():
+            for t in self.target:
+                if t in name:
+                    grad = param.grad
+                    with cuda.get_device(grad):
+                        grad = 0
+
 def main():
     print('Chainer Version: ' + chainer.__version__)
 
     parser = argparse.ArgumentParser(description='NN for Grayscale Image colorization')
     parser.add_argument('--batchsize', '-b', type=int, default=50, help='Number of images in each mini-batch')
-    parser.add_argument('--epoch', '-e', type=int, default=400, help='Number of sweeps over the dataset to train')
+    parser.add_argument('--epoch_class', type=int, default=200, help='Number of sweeps over the dataset to train(classification model)')
+    parser.add_argument('--epoch_color', '-e', type=int, default=400, help='Number of sweeps over the dataset to train')
     parser.add_argument('--dataset', '-d', default='./train', help='Directory of image files. Default is ./train')
     parser.add_argument('--out', '-o', default='./output', help='Directory to output the result')
     parser.add_argument('--gpu', '-g', type=int, default=-1, help='GPU ID (native value indicates CPU)')
-    parser.add_argument('--alpha', type=float, default=0.001, help='Learing rate for Adam')
-    parser.add_argument('--beta1', type=float, default=0.9, help='Momentum term of Adam')
-    parser.add_argument('--beta2', type=float, default=0.999, help='Momentum term of Adam')
-    parser.add_argument('--mapsize', type=int, default=2, help='Base size of convolution map')
+    parser.add_argument('--mapsize', type=int, default=8, help='Base size of convolution map')
     parser.add_argument('--snapshot', action='store_const', const=True, default=False, help='Take snapshot of the trainer/model/optimizer')
     parser.add_argument('--no_out_image', action='store_const', const=True, default=False)
     parser.add_argument('--no_print_log', action='store_const', const=True, default=False)
+    parser.add_argument('--del_grad', action='store_const', const=True, default=False)
     args = parser.parse_args()
 
     # Set up a neural network to train
-    model = M.Evalution(M.Colorization(args.mapsize, 3))
+    model_class = L.Classifier(M.Classification(args.mapsize, 3))
 
     # Setup an optimizer
-    def make_optimizer(model, alpha=0.001, beta1=0.9, beta2=0.999):
-        optimizer = chainer.optimizers.Adam(alpha=alpha, beta1=beta1, beta2=beta2)
+    def make_optimizer(model, optimizer=chainer.optimizers.Adam()):
         optimizer.setup(model)
         return optimizer
-    optimizer = make_optimizer(model, args.alpha, args.beta1, args.beta2)
+    opt_class = make_optimizer(model_class, chainer.optimizers.AdaGrad())
 
     # Print parameters
-    print('# Epoch:     {args.epoch}'.format(**locals()))
-    print('# BatchSize: {args.batchsize}'.format(**locals()))
-    print('# Alpha:     {args.alpha}'.format(**locals()))
-    print('# Beta1:     {args.beta1}'.format(**locals()))
-    print('# Beta2:     {args.beta2}'.format(**locals()))
-    print('# Mapsize:   {args.mapsize}'.format(**locals()))
-    print('# Dataset:   {args.dataset}'.format(**locals()))
+    print('# Epoch Class: {args.epoch_class}'.format(**locals()))
+    print('# Epoch Color: {args.epoch_color}'.format(**locals()))
+    print('# BatchSize:   {args.batchsize}'.format(**locals()))
+    print('# Mapsize:     {args.mapsize}'.format(**locals()))
+    print('# Dataset:     {args.dataset}'.format(**locals()))
 
     # Create Output directory
     output_dir = args.out
     if args.out == './output':
         output_dir = util.next_dir(args.out)
     util.make_dir(output_dir)
-    print('# Output:    {output_dir}'.format(**locals()))
+    print('# Output:      {output_dir}'.format(**locals()))
 
     # Setup for GPU
     xp = np
     if args.gpu >= 0:
         chainer.cuda.get_device_from_id(args.gpu).use()
-        model.to_gpu()
+        model_class.to_gpu()
         xp = chainer.cuda.cupy
-        print('# GPU:       {args.gpu}'.format(**locals()))
+        print('# GPU:         {args.gpu}'.format(**locals()))
 
     # Load the dataset
+    seed = random.randint(0, 100)
+    print('# Seed         {seed}'.format(**locals()))
     print('Data Loading...')
     start = time.time()
-    d = util.make_dataset(args.dataset)
-    train, test = datasets.split_dataset_random(d, int(len(d) * 0.9))
+    data_class, data_color = util.make_dataset(args.dataset)
+    train_class, test_class = datasets.split_dataset_random(data_class, int(len(data_class) * 0.9), seed=seed)
+    train_color, test_color = datasets.split_dataset_random(data_color, int(len(data_color) * 0.9), seed=seed)
     diff = time.time() - start
     print('Finish Loading: {diff}s'.format(**locals()))
 
     # Setup iterater
-    train_itr = chainer.iterators.SerialIterator(train, args.batchsize)
-    test_itr = chainer.iterators.SerialIterator(test, args.batchsize, repeat=False, shuffle=False)
+    train_itr_class = chainer.iterators.SerialIterator(train_class, args.batchsize)
+    test_itr_class  = chainer.iterators.SerialIterator(test_class, args.batchsize, repeat=False, shuffle=False)
 
     # Setup trainer
-    updater = training.StandardUpdater(train_itr, optimizer, device=args.gpu)
-    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=output_dir)
-    trainer.extend(extensions.LogReport(log_name='log.json'))
-    trainer.extend(extensions.Evaluator(test_itr, model, device=args.gpu))
-    trainer.extend(extensions.dump_graph(root_name='main/loss', out_name='cg.dot'))
-    if not args.no_print_log:
-        trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'validation/main/loss', 'main/accuracy', 'validation/main/accuracy', 'elapsed_time']))
-        trainer.extend(extensions.ProgressBar())
-    if args.snapshot:
-        trainer.extend(extensions.snapshot())
-        trainer.extend(extensions.snapshot_object(model, 'model_snapshot.model'))
-        trainer.extend(extensions.snapshot_object(optimizer, 'optimizer_snapshot.state'))
-    if extensions.PlotReport.available():
-        trainer.extend(extensions.PlotReport(['main/loss', 'validation/main/loss'], 'epoch', file_name='loss.png', marker=None))
-        trainer.extend(extensions.PlotReport(['main/accuracy', 'validation/main/accuracy'], 'epoch', file_name='accuracy.png', marker=None))
+    updater_class = training.StandardUpdater(train_itr_class, opt_class, device=args.gpu)
+    trainer_class = training.Trainer(updater_class, (args.epoch_class, 'epoch'), out=output_dir)
+    trainer_class.extend(extensions.LogReport(log_name='log_class.json'))
+    trainer_class.extend(extensions.Evaluator(test_itr_class, model_class, device=args.gpu))
+    trainer_class.extend(extensions.dump_graph(root_name='main/loss', out_name='cg_class.dot'))
 
-    trainer.run()
+    if not args.no_print_log:
+        trainer_class.extend(extensions.PrintReport(['epoch', 'main/loss', 'validation/main/loss', 'main/accuracy', 'validation/main/accuracy', 'elapsed_time']))
+        trainer_class.extend(extensions.ProgressBar())
+    if args.snapshot:
+        trainer_class.extend(extensions.snapshot())
+        trainer_class.extend(extensions.snapshot_object(model_class, 'model_class_snapshot.model'))
+        trainer_class.extend(extensions.snapshot_object(opt_class, 'optimizer_class_snapshot.state'))
+    if extensions.PlotReport.available():
+        trainer_class.extend(extensions.PlotReport(['main/loss', 'validation/main/loss'], 'epoch', file_name='loss_class.png', marker=None))
+        trainer_class.extend(extensions.PlotReport(['main/accuracy', 'validation/main/accuracy'], 'epoch', file_name='accuracy_class.png', marker=None))
+
+    trainer_class.run()
+
+
+    # Set up a neural network to train
+    net = opt_class.target.predictor.to_cpu()
+    model_color = M.Evalution(M.Colorization(args.mapsize, 3, net))
+
+    # Setup an optimizer
+    opt_color = make_optimizer(model_color, chainer.optimizers.Adam())
+
+    if args.del_grad:
+        opt_color.add_hook(DelGrad(['conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'conv6', 'conv7', 'conv8']))
+
+    # Setup for GPU
+    if args.gpu >= 0:
+        model_color.to_gpu()
+
+    # Setput iterator
+    train_itr_color = chainer.iterators.SerialIterator(train_color, args.batchsize)
+    test_itr_color  = chainer.iterators.SerialIterator(test_color, args.batchsize, repeat=False, shuffle=False)
+
+    # Setup trainer
+    updater_color = training.StandardUpdater(train_itr_color, opt_color, device=args.gpu)
+    trainer_color = training.Trainer(updater_color, (args.epoch_color, 'epoch'), out=output_dir)
+    trainer_color.extend(extensions.LogReport(log_name='log_color.json'))
+    trainer_color.extend(extensions.Evaluator(test_itr_color, model_color, device=args.gpu))
+    trainer_color.extend(extensions.dump_graph(root_name='main/loss', out_name='cg_class.dot'))
+
+    if not args.no_print_log:
+        trainer_color.extend(extensions.PrintReport(['epoch', 'main/loss', 'validation/main/loss', 'main/accuracy', 'validation/main/accuracy', 'elapsed_time']))
+        trainer_color.extend(extensions.ProgressBar())
+    if args.snapshot:
+        trainer_color.extend(extensions.snapshot())
+        trainer_color.extend(extensions.snapshot_object(model_color, 'model_color_snapshot.model'))
+        trainer_color.extend(extensions.snapshot_object(opt_color, 'optimizer_color_snapshot.state'))
+    if extensions.PlotReport.available():
+        trainer_color.extend(extensions.PlotReport(['main/loss', 'validation/main/loss'], 'epoch', file_name='loss_color.png', marker=None))
+        trainer_color.extend(extensions.PlotReport(['main/accuracy', 'validation/main/accuracy'], 'epoch', file_name='accuracy_color.png', marker=None))
+
+    trainer_color.run()
 
     # Save model/optimizer
     date = datetime.datetime.today().strftime("%m-%d_%H%M")
-    serializers.save_npz('{output_dir}/{date}.state'.format(**locals()), optimizer)
+    serializers.save_npz('{output_dir}/{date}_class.state'.format(**locals()), opt_class)
+    serializers.save_npz('{output_dir}/{date}_color.state'.format(**locals()), opt_color)
     if args.gpu >= 0:
-        serializers.save_npz('{output_dir}/{date}_gpu.model'.format(**locals()), model)
-        serializers.save_npz('{output_dir}/{date}_cpu.model'.format(**locals()), copy.deepcopy(model).to_cpu())
+        serializers.save_npz('{output_dir}/{date}_class_gpu.model'.format(**locals()), model_class)
+        serializers.save_npz('{output_dir}/{date}_color_gpu.model'.format(**locals()), model_color)
+        serializers.save_npz('{output_dir}/{date}_class_cpu.model'.format(**locals()), copy.deepcopy(model_class).to_cpu())
+        serializers.save_npz('{output_dir}/{date}_color_cpu.model'.format(**locals()), copy.deepcopy(model_color).to_cpu())
     else:
-        serializers.save_npz('{output_dir}/{date}_cpu.model'.format(**locals()), model)
+        serializers.save_npz('{output_dir}/{date}_class_cpu.model'.format(**locals()), model_class)
+        serializers.save_npz('{output_dir}/{date}_color_cpu.model'.format(**locals()), model_color)
     print('model/optimizer Saved: {output_dir}/{date}.*'.format(**locals()))
 
     # Predict test images and Save output images
     if not args.no_out_image:
         chainer.using_config('train', False)
-        data_n = len(test)
-        out_itr = chainer.iterators.SerialIterator(test, 1, repeat=False, shuffle=False)
+        data_n = len(test_color)
+        out_itr = chainer.iterators.SerialIterator(test_color, 1, repeat=False, shuffle=False)
         for i in range(data_n):
             x = out_itr.next().__getitem__(0)[0]
-            y = model.predictor(xp.asarray([x]))
+            y = model_color.predictor(xp.asarray([x]))
             if args.gpu >= 0:
                 img = util.output2img(chainer.cuda.to_cpu(y.data)[0])
             else:
